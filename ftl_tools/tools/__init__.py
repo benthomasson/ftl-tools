e59d47530aff97514181f821497a752ca8fb3263
Ben Thomasson <ben.thomasson@gmail.com>
2025-05-09 09:41:34 -0400

    Add timezone tool

--- ftl_tools/tools/__init__.py
+++ ftl_tools/tools/__init__.py
@@ -16,54 +16,10 @@ from rich.rule import Rule
 
 from ftl_tools.utils import dependencies, display_results, display_tool
 
+from .timezone import Timezone
+
 
 logger = logging.getLogger("tools")
-
-dependencies = [
-    "ftl_module_utils @ git+https://github.com/benthomasson/ftl_module_utils@main",
-    "ftl_collections @ git+https://github.com/benthomasson/ftl_collections@main",
-]
-
-
-def write_or_print(output, console, log):
-
-    if log is None:
-        console.print(output)
-    else:
-        log.write(output)
-
-
-def display_tool(tool, console, log):
-    write_or_print(
-        Rule(title=f"\n[green]TOOL [white] [ {tool.name} ]", align="left"),  # noqa: W605
-        console,
-        log,
-    )
-
-
-def display_results(output, console, log):
-    if log is None:
-        pprint(output, console=console)
-        for name, results in output.items():
-            if results.get("failed"):
-                raise Exception(results.get("msg"))
-            if results.get("changed"):
-                console.print(f"[yellow] changed: [{name}]")
-            else:
-                console.print(f"[green] ok: [{name}]")
-        console.print("")
-        console.print_json(json.dumps(output))
-    else:
-        log.write(output)
-        for name, results in output.items():
-            if results.get("failed"):
-                raise Exception(results.get("msg"))
-            if results.get("changed"):
-                log.write(f"[yellow] changed: [{name}]")
-            else:
-                log.write(f"[green] ok: [{name}]")
-        log.write("")
-        log.write(json.dumps(output))
 
 
 class Service(Tool):
@@ -975,6 +931,7 @@ __all__ = [
     "Unarchive",
     "Mkdir",
     "JavaJar",
+    "Timezone",
 ]
 
 
