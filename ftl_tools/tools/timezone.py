51e331c6d0b52b7e0d403ef1c4a1146b8f5b0b94
Ben Thomasson <ben.thomasson@gmail.com>
2025-05-09 09:40:02 -0400

    Add timezone tool

---
+++ ftl_tools/tools/timezone.py
@@ -0,0 +1,41 @@
+from smolagents.tools import Tool
+from ftlagents.tools import get_json_schema
+import faster_than_light as ftl
+from ftl_tools.utils import dependencies, display_results, display_tool
+
+
+class Timezone(Tool):
+    name = "timezone"
+    module = "timezone"
+
+    def __init__(self, state, *args, **kwargs):
+        self.state = state
+        super().__init__(*args, **kwargs)
+
+    def forward(self, name: str) -> bool:
+        '''Configure timezone setting
+
+        Args:
+            name: Name of the timezone for the system clock.
+
+        Returns:
+            boolean
+        '''
+        display_tool(self, self.state["console"], self.state["log"])
+
+        output = ftl.run_module_sync(
+            self.state["inventory"],
+            self.state["modules"],
+            self.module,
+            self.state["gate_cache"],
+            module_args=dict(name=name),
+            dependencies=dependencies,
+            loop=self.state["loop"],
+            use_gate=self.state["gate"],
+        )
+
+        display_results(output, self.state["console"], self.state["log"])
+
+        return output
+
+    description, inputs, output_type = get_json_schema(forward)
